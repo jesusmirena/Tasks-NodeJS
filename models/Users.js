e134f7dbee5b652eb9041cd48e9793444a675c72
jesusmirena <jesusamh16@gmail.com>
2021-12-06 00:57:09 -0500

    Hashing passwords with Bcrypt

--- models/Users.js
+++ models/Users.js
@@ -1,22 +1,33 @@
 const Sequelize = require("sequelize");
 const db = require("../config/db");
 const Projects = require("./Projects");
+const bcrypt = require("bcrypt");
 
-const Users = db.define("users", {
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const Users = db.define(
+  "users",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    email: {
+      type: Sequelize.STRING(60),
+      allowNull: false,
+    },
+    password: {
+      type: Sequelize.STRING(60),
+      allowNull: false,
+    },
   },
-  email: {
-    type: Sequelize.STRING(60),
-    allowNull: false,
-  },
-  password: {
-    type: Sequelize.STRING(60),
-    allowNull: false,
-  },
-});
+  {
+    hooks: {
+      beforeCreate(user) {
+        user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10));
+      },
+    },
+  }
+);
 Users.hasMany(Projects);
 
 module.exports = Users;
