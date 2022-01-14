b41fb9c5a7f98ed88807d1c8c6475e20d37270cc
jesusmirena <jesusamh16@gmail.com>
2022-01-14 14:42:59 -0500

    accessing user information

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ app.use(passport.session());
 app.use((req, res, next) => {
   res.locals.vardump = helpers.vardump;
   res.locals.messages = req.flash();
+  res.locals.user = { ...req.user } || null;
   next();
 });
 
